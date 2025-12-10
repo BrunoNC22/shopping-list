9de2d51ef773567a826983ddbc26dcc7a622c3ee
BrunoNC22 <thebegone2@gmail.com>
2025-12-10 01:25:33 -0300

    refactor: move main.tsx to main folder and rename it to index.tsx

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,14 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './style.css'
+import './presentation/style/global.css'
+import Router from './routes/router'
 import { ThemeProvider } from './providers/theme/ThemeProvider'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider defaultTheme='dark' storageKey='app-theme'>
-      <h1 className="text-3xl font-bold underline text-primary">
-        Hello world!
-      </h1>
+      <Router />
     </ThemeProvider>
   </StrictMode>,
 )
