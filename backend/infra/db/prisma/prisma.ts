e8eb7dbe67cbb0f2b8643e5ff4b196ffeb687741
BrunoNC22 <thebegone2@gmail.com>
2026-03-13 01:37:48 -0300

    feat: create prisma files

---
+++ backend/infra/db/prisma/prisma.ts
@@ -0,0 +1,11 @@
+import "dotenv/config";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "./generated/client";
+
+
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const adapter = new PrismaPg({ connectionString });
+// Prevent hot reloading from creating new instances of PrismaClient https://www.prisma.io/docs/orm/prisma-client/setup-and-configuration/databases-connections#prevent-hot-reloading-from-creating-new-instances-of-prismaclient
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
